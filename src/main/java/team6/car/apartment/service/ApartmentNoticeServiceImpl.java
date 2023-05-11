7213131a320458cbb531081a67008af6886b16ba
DDeo99 <yoondukwoo@gmail.com>
2023-05-11 15:31:36 +0900

    아파트 공지사항 조회 Query 수정

--- src/main/java/team6/car/apartment/service/ApartmentNoticeServiceImpl.java
+++ src/main/java/team6/car/apartment/service/ApartmentNoticeServiceImpl.java
@@ -8,6 +8,8 @@ import team6.car.apartment.DTO.ApartmentNoticeDto;
 import team6.car.apartment.domain.ApartmentNotice;
 import team6.car.apartment.repository.ApartmentNoticeRepository;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -18,7 +20,7 @@ public class ApartmentNoticeServiceImpl implements ApartmentNoticeService{
 
     @Override
     public ApartmentNoticeDto getApartmentNotice(Long id) {
-        ApartmentNotice apartmentNotice = apartmentNoticeRepository.findById(id)
+        ApartmentNotice apartmentNotice = apartmentNoticeRepository.findByApartmentId(id)
                 .orElseThrow(() -> new RuntimeException("아파트 정보를 찾을 수 없습니다."));
 
         return ApartmentNoticeDto.builder()
@@ -26,5 +28,10 @@ public class ApartmentNoticeServiceImpl implements ApartmentNoticeService{
                 .apartment_notice_date(apartmentNotice.getApartment_notice_date())
                 .notice(apartmentNotice.getApartment_notice())
                 .build();
+    }
+
+    @Override
+    public Optional<ApartmentNotice> findByApartmentId(Long apartment_id) {
+        return apartmentNoticeRepository.findByApartmentId(apartment_id);
     }
 }

ea14a8885a2fab75b6c52779c6a4999000f06150
DDeo99 <yoondukwoo@gmail.com>
2023-05-17 03:40:00 +0900

    ApartmentDto Swagger 설명 추가

--- src/main/java/team6/car/apartment/DTO/ApartmentDto.java
+++ src/main/java/team6/car/apartment/DTO/ApartmentDto.java
@@ -1,5 +1,7 @@
 package team6.car.apartment.DTO;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApartmentDto {
+    @ApiModelProperty(example = "푸르지오")
+    @ApiParam(value = "아파트 이름")
     private String name;
 
 }

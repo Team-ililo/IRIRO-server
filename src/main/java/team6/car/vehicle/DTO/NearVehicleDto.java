860f4224efcec55f4c1405d7e09ed180328db775
DW <yoondukwoo@gmial.com>
2023-05-17 03:38:25 +0900

    VehicleDto Swagger 설명 추가

--- src/main/java/team6/car/vehicle/DTO/NearVehicleDto.java
+++ src/main/java/team6/car/vehicle/DTO/NearVehicleDto.java
@@ -1,5 +1,7 @@
 package team6.car.vehicle.DTO;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -9,10 +11,22 @@ import java.time.LocalTime;
 @Setter
 @Builder
 public class NearVehicleDto {
+    @ApiModelProperty(example = "A123456")
+    @ApiParam(value = "차량 번호")
     private String vehicle_number;
+    @ApiModelProperty(example = "마차")
+    @ApiParam(value = "차량 모델")
     private String model;
+    @ApiModelProperty(example = "검은색")
+    @ApiParam(value = "차량 색상")
     private String color;
+    @ApiModelProperty(example = "08:30:00")
+    @ApiParam(value = "출차 시간 hh:mm:ss")
     private String exitTime;
+    @ApiModelProperty(example="장기 주차 여부")
+    @ApiParam(value = "false")
     private boolean isLongTermParking;
+    @ApiModelProperty(example = "true")
+    @ApiParam(value = "해당 차량 앞에 이중주차를 해도 되는가")
     private boolean isSatisfied;
 }
--- src/main/java/team6/car/vehicle/DTO/VehicleDto.java
+++ src/main/java/team6/car/vehicle/DTO/VehicleDto.java
@@ -1,5 +1,7 @@
 package team6.car.vehicle.DTO;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,11 +14,23 @@ import java.time.LocalTime;
 @Setter
 @Builder
 public class VehicleDto {
+    @ApiModelProperty(example = "B123456")
+    @ApiParam(value = "차량 번호")
     private String vehicle_number;
+    @ApiModelProperty(example = "마차")
+    @ApiParam(value = "차량 모델")
     private String model;
+    @ApiModelProperty(example = "검정색")
+    @ApiParam(value = "차량 색상")
     private String color;
+    @ApiModelProperty(example = "08:40:00")
+    @ApiParam(value = "출차 시간 hh:mm:ss")
     private LocalTime exitTime;
+    @ApiModelProperty(example = "false")
+    @ApiParam(value = "장기 주차 여부")
     private boolean isLongTermParking;
+    @ApiModelProperty(example = "101동 102호")
+    @ApiParam(value = "아파트 동/호수")
     private String address;
 
     public VehicleDto() {

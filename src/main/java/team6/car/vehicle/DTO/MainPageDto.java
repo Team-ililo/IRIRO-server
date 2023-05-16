fa97da9205e206b1e717bb328c81b5c4b701fd57
DDeo99 <yoondukwoo@gmail.com>
2023-05-17 03:38:25 +0900

    VehicleDto Swagger 설명 추가

--- src/main/java/team6/car/vehicle/DTO/MainPageDto.java
+++ src/main/java/team6/car/vehicle/DTO/MainPageDto.java
@@ -1,5 +1,7 @@
 package team6.car.vehicle.DTO;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,10 +11,20 @@ import java.time.LocalTime;
 @Setter
 @Builder
 public class MainPageDto {
+    @ApiModelProperty(example = "08:30")
+    @ApiParam(value = "출차 시간 hh:mm")
     private String formattedExitTime;
+    @ApiModelProperty(example = "12:15")
+    @ApiParam(value = "출차 시간까지의 남은 시간 hh:mm")
     private String remainingTime;
+    @ApiModelProperty(example = "false")
+    @ApiParam(value = "장기 주차 여부")
     private boolean isLongTermParking;
+    @ApiModelProperty(example = "푸르지오")
+    @ApiParam(value = "아파트 이름")
     private String apartmentName;
+    @ApiModelProperty(example = "101동 101호")
+    @ApiParam(value = "아파트 동/호수")
     private String address;
 
 }

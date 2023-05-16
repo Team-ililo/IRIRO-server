32cc3c5829127a1bfebd78d2d1973777988aafb7
DDeo99 <yoondukwoo@gmail.com>
2023-05-17 02:16:40 +0900

    swagger 기본 세팅

--- src/main/java/team6/car/SwaggerConfig.java
+++ src/main/java/team6/car/SwaggerConfig.java
@@ -3,36 +3,57 @@ package team6.car;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("team6.car"))
-                // [Group + Artifact] ex) com.example.demo
-                .paths(PathSelectors.any())
-                .build();
-    }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot Open API TEST with swagger")
-                .description("Swagger를 사용한 Spring Boot Open API 테스트")
-                .version("1.0.0")
-                .build();
-    }
+        private static final String API_NAME = "Study API";
+        private static final String API_VERSION = "0.0.1";
+        private static final String API_DESCRIPTION = "Study API 명세서";
+
+        @Bean
+        public Docket api() {
+            Parameter parameterBuilder = new ParameterBuilder()
+                    .name(HttpHeaders.AUTHORIZATION)
+                    .description("Access Tocken")
+                    .modelRef(new ModelRef("string"))
+                    .parameterType("header")
+                    .required(false)
+                    .build();
+
+            List<Parameter> globalParamters = new ArrayList<>();
+            globalParamters.add(parameterBuilder);
+
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .globalOperationParameters(globalParamters)
+                    .apiInfo(apiInfo())
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("team6.car"))
+                    .paths(PathSelectors.any())
+                    .build();
+        }
+
+        public ApiInfo apiInfo() {
+            return new ApiInfoBuilder()
+                    .title(API_NAME)
+                    .version(API_VERSION)
+                    .description(API_DESCRIPTION)
+                    .build();
+        }
 }

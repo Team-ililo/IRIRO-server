0ed042f4dcb72c7fe5d657f9a18b750568912412
DW <yoondukwoo@gmial.com>
2023-05-17 03:40:00 +0900

    ApartmentDto Swagger 설명 추가

--- src/main/java/team6/car/apartment/DTO/ApartmentNoticeDto.java
+++ src/main/java/team6/car/apartment/DTO/ApartmentNoticeDto.java
@@ -1,5 +1,7 @@
 package team6.car.apartment.DTO;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApartmentNoticeDto {
+    @ApiModelProperty(example = "2023-05-17T9:30:00")
+    @ApiParam(value = "아파트 공지 시간")
     private LocalDateTime apartment_notice_date;
+    @ApiModelProperty(example = "공지사항 내용")
+    @ApiParam(value = "아파트 공지사항")
     private String notice;
 }

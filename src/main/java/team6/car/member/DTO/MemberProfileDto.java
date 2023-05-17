92819f4e673bff20fff8201b271dc85c82a97d9d
DDeo99 <yoondukwoo@gmail.com>
2023-05-17 15:13:55 +0900

    MemberDto Swagger 설명 추가

--- src/main/java/team6/car/member/DTO/MemberProfileDto.java
+++ src/main/java/team6/car/member/DTO/MemberProfileDto.java
@@ -1,5 +1,7 @@
 package team6.car.member.DTO;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.*;
 
 @Data
@@ -9,8 +11,16 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberProfileDto {
+    @ApiModelProperty(example = "IRIRO@capstone.com")
+    @ApiParam(value = "이메일 아이디")
     private String email;
+    @ApiModelProperty(example = "A123123")
+    @ApiParam(value = "차량 번호")
     private String vehicle_number;
+    @ApiModelProperty(example = "101동 103호")
+    @ApiParam(value = "아파트 동/호수")
     private String address;
+    @ApiModelProperty(example = "0")
+    @ApiParam(value = "신고 당한 횟수")
     private int number_of_complaints;
 }
--- src/main/java/team6/car/member/DTO/getReportDto.java
+++ src/main/java/team6/car/member/DTO/getReportDto.java
@@ -1,5 +1,7 @@
 package team6.car.member.DTO;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.*;
 
 @Getter
@@ -8,6 +10,10 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class getReportDto {
+    @ApiModelProperty(example = "등록한 출차 시간 이전에 차를 빼달라고 요구함")
+    @ApiParam(value = "신고 내용")
     private String complaint_contents;
+    @ApiModelProperty(example = "1")
+    @ApiParam(value = "신고 당한 횟수")
     private int number_of_complaints;
 }

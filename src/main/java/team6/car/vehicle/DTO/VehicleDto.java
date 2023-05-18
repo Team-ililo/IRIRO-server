1ffcdbb16a702ba576a9209d0eafc538a6ab4c83
DDeo99 <yoondukwoo@gmail.com>
2023-05-18 16:30:11 +0900

    VehicleDto 수정

--- src/main/java/team6/car/vehicle/DTO/VehicleDto.java
+++ src/main/java/team6/car/vehicle/DTO/VehicleDto.java
@@ -14,6 +14,7 @@ import java.time.LocalTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VehicleDto {
+    /*
     @ApiModelProperty(example = "B123456")
     @ApiParam(value = "차량 번호")
     private String vehicle_number;
@@ -23,13 +24,15 @@ public class VehicleDto {
     @ApiModelProperty(example = "검정색")
     @ApiParam(value = "차량 색상")
     private String color;
+    @ApiModelProperty(example = "101동 102호")
+    @ApiParam(value = "아파트 동/호수")
+    private String address;
+    */
+
     @ApiModelProperty(example = "08:40:00")
     @ApiParam(value = "출차 시간 hh:mm:ss")
     private LocalTime exitTime;
     @ApiModelProperty(example = "false")
     @ApiParam(value = "장기 주차 여부")
     private Boolean no_departure;
-    @ApiModelProperty(example = "101동 102호")
-    @ApiParam(value = "아파트 동/호수")
-    private String address;
 }

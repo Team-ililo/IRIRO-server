ee414f8823773f4e82c02b4c12589f2200b7de46
DW <yoondukwoo@gmial.com>
2023-05-21 18:39:21 +0900

    로그인 response에 아파트 이름 추가

--- src/main/java/team6/car/member/DTO/ReturnId.java
+++ src/main/java/team6/car/member/DTO/ReturnId.java
@@ -15,8 +15,8 @@ public class ReturnId {
     @ApiParam(value = "멤버 아이디")
     private Long member_id;
     @ApiModelProperty(example = "1")
-    @ApiParam(value = "아파트 아이디")
-    private Long apartment_id;
+    @ApiParam(value = "아파트 이름")
+    private String apartment_name;
     @ApiModelProperty(example = "1")
     @ApiParam(value = "디바이스 아이디")
     private Long device_id;
